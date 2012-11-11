32aeb5333bfefbbfaff8dbb528a4da78fb4a9d0f
hiromitsu matsuoka <nufan5802@gmail.com>
2012-11-11 18:57:04 +0900

    modified php recipe.

--- recipes/php.rb
+++ recipes/php.rb
@@ -1,4 +1,11 @@
 include_recipe "pivotal_workstation::homebrew"
 
-brew_install "php53"
+brew_install "zlib"
+brew_install "freetype"
+brew_install "jpeg"
+brew_install "libpng"
+brew_install "gd"
+brew_install "gettext"
+brew_install "mcrypt"
+brew_install("php53", :brew_args => "--with-mysql --with-apache")
 brew_install "php53-xdebug"
